2fc0e37663e60725f62a650ff153d28a300c2941
welagaieric <welangaieric@gmail.com>
2024-08-30 02:19:03 +0300

    new commit change

--- JS/details.js
+++ JS/details.js
@@ -43,11 +43,28 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((product) => {
             console.log(product); 
             let display = $('#checkout')
-            let temp = `<div class="checkout d-flex center fd-col">
+            let temp = `
+                        <div class="row d-flex center">
+                            <h2 class="text-black">Product Details</h2>
+                        </div>
+                        <div class="checkout d-flex jcs aic fd-col">
                             <div class="checkout-header d-flex center">
                                 <img src=${product.image} alt=${JSON.stringify(product.name)}>
                             </div>
-                            <div class="checkout-body d-flex">
+                             <div class="checkout-body jcs aic g-1 d-flex fd-col ">
+                               
+                                <div class="row strain d-flex left g-1">
+                                    <h3>Name:</h3>
+                                    <p>${product.name}</p>
+                                </div>
+                                <div class="row strain d-flex left g-1">
+                                    <h3>Description:</h3>
+                                    <p>${product.description}</p>
+                                </div>
+                                <div class="row strain d-flex left g-1">
+                                    <h3>Price:</h3>
+                                    <p>${product.price}</p>
+                                </div>
 
                             </div>
                         </div>`
